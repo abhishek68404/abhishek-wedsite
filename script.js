7c685f338c11a3939cc3edac77cf6506559e2f46
abhishek68404 <as9834068404@gmail.com>
2026-06-03 21:07:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -267,3 +267,37 @@ card.style.transform=
 
     }
 );
+const counters =
+document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+const updateCounter=()=>{
+
+const target=
++counter.getAttribute("data-target");
+
+const count=
++counter.innerText;
+
+const increment=
+target/100;
+
+if(count<target){
+
+counter.innerText=
+Math.ceil(count+increment);
+
+setTimeout(updateCounter,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+};
+
+updateCounter();
+
+});
